74be0b0f19d38389988c5c2fa735e2f55f284188
TheNabo1 <soulier.clement@me.com>
2014-02-17 14:54:19 +0100

    relou2

--- src/javacommun/Traducteur.java
+++ src/javacommun/Traducteur.java
@@ -4,50 +4,49 @@
  */
 package javacommun;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  *
  * @author Thomas
  */
 public class Traducteur {
-    private Map listMotsCles;
+    private HashMap phraseATraduire;
 
-    public Traducteur(List<Service> services) {
-        Map match = new HashMap();
-        for(Service i : services){
-            match.put(i, i.getMotCle());
-        }
-        this.listMotsCles = match;
+    public Traducteur(HashMap phraseATraduire) {
+        this.phraseATraduire = phraseATraduire;
     }
 
-    public List Traduire(String phrase){
-        String[] listMots = phrase.split(" ");
-        List servicesMatch = new ArrayList();
-        Iterator iteratorlistMotsCles = listMotsCles.iterator();
-        while (iteratorlistMotsCles.hasNext()){
-            HashMap currentMotCle = (HashMap)iteratorlistMotsCles.next();
-            for (int i = 0; listMots.length <= i; i++){
-                if (listMots[i] == currentMotCle.getValue().getMotCle()){
-                    servicesMatch.add(currentMotCle);
+    public void Traduire(){/*
+        Service service = new Service();
+        HashMap listeService = service.getAll();
+        Set clesListeService = listeService.keySet();
+        Iterator iteratorListeService = clesListeService.iterator();
+        while (iteratorListeService.hasNext()){
+           Object cleListeServices = iteratorListeService.next(); // tu peux typer plus finement ici
+           HashMap listeMotsCles = listeService.get(cleListeServices); // tu peux typer plus finement ici
+            K cles = listeMotsCles.keySet();
+            Iterator it = cles.iterator();
+            while (it.hasNext()){
+               Object cle = it.next(); // tu peux typer plus finement ici
+               String valeurMotCle = listeMotsCles.get(cle); // tu peux typer plus finement ici
+               K clefs = this.phraseATraduire.keySet();
+                Iterator ite = clefs.iterator();
+                while (ite.hasNext()){
+                   Object clef = ite.next(); // tu peux typer plus finement ici
+                   Object valeurPhrase = this.phraseATraduire.get(clef); // tu peux typer plus finement ici
+                   if (valeurMotCle == valeurPhrase){
+                       this.EnvoyerRequete((Service) valeurMotCle);
+                   }
+
                 }
             }
         }
-        this.EnvoyerRequete(servicesMatch);
-        return servicesMatch;
+        return null;*/
     }
     
-    public void EnvoyerRequete(List service){
-        Iterator it = service.iterator();
-        for (it.hasNext()){
-              HashMap map = (HashMap) it.next();
-              map.getKey().comprendre(map.getValue().getKey());
-        }
+    public void EnvoyerRequete(/*Service service*/){
+        
     }
     
     
